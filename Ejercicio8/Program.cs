2dfb1eaf02da0a3705b53d1f07f499310ca8de21
Adrián García Riera <i7610@softgal.com>
2018-01-26 13:01:22 +0100

    Últimos ejercicios 26/01/18

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
