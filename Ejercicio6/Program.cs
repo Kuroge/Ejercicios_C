293abeb1c73cf15fc6a6f377b678a41fb0ae0ef9
Adrián García Riera <i7610@softgal.com>
2018-01-26 12:00:55 +0100

    Primer ejercicio un poco largo

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el precio del artículo");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Introduce el método de pago: efectivo o tarjeta");
+            string mpago = (Console.ReadLine()).ToLower();
+
+            if (mpago.Equals("efectivo"))
+            {
+                Console.WriteLine($"Se ha pagado el producto x con un precio de {price} mediante {mpago}");
+                Console.ReadKey();
+            }else if (mpago.Equals("tarjeta"))
+            {
+                Console.WriteLine("Por favor, introduce un número de cuenta");
+                int n_cuenta = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Se ha pagado el producto x con un precio de {price} mediante {mpago} con número de cuenta {n_cuenta}");
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.WriteLine("Método de pago incorrecto");
+                Console.ReadKey();
+            }
+        }
+    }
+}
