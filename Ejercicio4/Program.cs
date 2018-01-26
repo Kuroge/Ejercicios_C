26607e118fc9e02bb38be940be64a0208a4927c0
Adrián García Riera <i7610@softgal.com>
2018-01-26 11:46:42 +0100

    Más ejercicios todavía

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el primer número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            if(num2 >= num1)
+            {
+                Console.WriteLine("El segundo número es mayor o igual que el primero");
+            }
+            else
+            {
+                Console.WriteLine("El primer número es mayor que el segundo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un día de la semana");
+            string dia = Console.ReadLine();
+
+            switch (dia.ToLower())
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "sabado":
+                case "domindo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+
+                default:
+                    Console.WriteLine("No has introducido un día de la semana o no lo has escrito bien");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
